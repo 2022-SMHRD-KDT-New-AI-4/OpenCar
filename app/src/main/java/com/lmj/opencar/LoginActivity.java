eb38e7878183d18d4d5ad75973be0d69d3a6e86a
miniz2 <mj21917@naver.com>
2023-04-24 16:38:15 +0900

    로그인수정중(민지)

--- app/src/main/java/com/lmj/opencar/LoginActivity.java
+++ app/src/main/java/com/lmj/opencar/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.lmj.opencar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -13,12 +15,32 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
     EditText edt_id,edt_pw;
     Button btn_login;
     CheckBox ch_auto;
     String loginId,loginPw;
     TextView tv_join;
+
+    StringRequest request_login;
+    RequestQueue queue;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +52,10 @@ public class LoginActivity extends AppCompatActivity {
         ch_auto =  findViewById(R.id.ch_auto);
         tv_join = findViewById(R.id.tv_join);
 
+        // 통로는 한개만 있어두 됨
+        queue = Volley.newRequestQueue(getApplicationContext());
 
+        // 회원가입 페이지로 이동
         tv_join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,10 +80,15 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         else if (loginId == null && loginPw == null) {
-            btn_login.setOnClickListener(new View.OnClickListener() {
+            String url = "http://218.157.24.41:5000/login/"+edt_id.getText().toString()+"/"+edt_pw.getText().toString();
+            request_login = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
                 @Override
-                public void onClick(View view) {
-                    if (edt_id.getText().toString().equals("hi") && edt_pw.getText().toString().equals("123") && ch_auto.isChecked()==true){
+                public void onResponse(String response) {
+
+                    try {
+                        JSONObject jo = new JSONObject(response);
+                        // 자동로그인 O
+                        if (ch_auto.isChecked()==true){
                         SharedPreferences auto = getSharedPreferences("auto",Activity.MODE_PRIVATE);
                         SharedPreferences.Editor autologin = auto.edit();
                         loginId = auto.getString("inputId",edt_id.getText().toString());
@@ -69,14 +99,56 @@ public class LoginActivity extends AppCompatActivity {
                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                         startActivity(intent);
                         finish();
-                    } else if (edt_id.getText().toString().equals("hi") && edt_pw.getText().toString().equals("123")) {
-                        loginId = auto.getString("inputId",edt_id.getText().toString());
-                        Toast.makeText(LoginActivity.this, loginId+"님 환영합니다", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                        intent.putExtra("id",edt_id.getText().toString());
-                        startActivity(intent);
-                        finish();
+                        } else {
+                            loginId = auto.getString("inputId",edt_id.getText().toString());
+                            Toast.makeText(LoginActivity.this, loginId+"님 환영합니다", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.putExtra("id",edt_id.getText().toString());
+                            startActivity(intent);
+                            finish();
+                        }
+
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), "ID/PW를 다시 확인해주세요", Toast.LENGTH_LONG).show();
+                        Log.d("herehere","Err onResponseJson: " + e.toString());
                     }
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+
+            btn_login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    queue.add(request_login);
+
+//                    // 자동로그인 O
+//                    if (edt_id.getText().toString().equals("hi") && edt_pw.getText().toString().equals("123") && ch_auto.isChecked()==true){
+//                        SharedPreferences auto = getSharedPreferences("auto",Activity.MODE_PRIVATE);
+//                        SharedPreferences.Editor autologin = auto.edit();
+//                        loginId = auto.getString("inputId",edt_id.getText().toString());
+//                        autologin.putString("inputId",edt_id.getText().toString());
+//                        autologin.putString("inputPw",edt_pw.getText().toString());
+//                        autologin.commit();
+//                        Toast.makeText(LoginActivity.this, loginId+"님 환영합니다", Toast.LENGTH_SHORT).show();
+//                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                        startActivity(intent);
+//                        finish();
+//                    }
+//                    // 자동로그인 X
+//                    else if (edt_id.getText().toString().equals("hi") && edt_pw.getText().toString().equals("123")) {
+//                        loginId = auto.getString("inputId",edt_id.getText().toString());
+//                        Toast.makeText(LoginActivity.this, loginId+"님 환영합니다", Toast.LENGTH_SHORT).show();
+//                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                        intent.putExtra("id",edt_id.getText().toString());
+//                        startActivity(intent);
+//                        finish();
+//                    }
                 }
             });
 
