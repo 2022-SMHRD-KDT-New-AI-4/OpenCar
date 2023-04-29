11082e0062a91cc189bc155b877f1f18efee35fa
Daeyoon <rkwk1220@gmail.com>
2023-04-29 18:12:05 +0900

    대윤- 안드로이드 (Main, driveActivity Volley 기능)

--- app/src/main/java/com/lmj/opencar/JoinActivity.java
+++ app/src/main/java/com/lmj/opencar/JoinActivity.java
@@ -7,8 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -27,6 +30,7 @@ public class JoinActivity extends AppCompatActivity {
     RequestQueue queue;
     StringRequest request_join;
     PortClass port;
+//    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +41,42 @@ public class JoinActivity extends AppCompatActivity {
         pw = findViewById(R.id.edt_pw);
         birth = findViewById(R.id.edt_bir);
         name = findViewById(R.id.edt_name);
+
+        //어댑터 생성
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinner_array,R.layout.activity_join);
+        //드롭다운뷰 연결
+        adapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        //UI와 연결
+//        binding.homeSpinner.setAdapter(adapter);
+    }
+
+    //Spinner Listener
+    public void spinnerListener() {
+//        binding.homeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//
+//            //선택 시 작동기능
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                switch (position) {
+//                    case 1:
+//                        break;
+//                    case 2:
+//                        break;
+//                    case 3:
+//                        break;
+//                }
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+
+
+
+
+
 
         queue = Volley.newRequestQueue(getApplicationContext());
 
