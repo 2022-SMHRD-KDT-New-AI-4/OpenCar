092bf1e964414efba686045537930149ded7c57e
Daeyoon <rkwk1220@gmail.com>
2023-05-01 11:09:13 +0900

    대윤- 안드로이드 (join 스피너 기능)

--- app/src/main/java/com/lmj/opencar/PortClass.java
+++ app/src/main/java/com/lmj/opencar/PortClass.java
@@ -1,5 +1,7 @@
 package com.lmj.opencar;
 
 public class PortClass {
-    static String port = "http://218.157.24.41:5001/";
+    //static String port = "http://218.157.24.41:5001/";
+
+    static String port = "http://211.63.240.23:5001/";
 }
