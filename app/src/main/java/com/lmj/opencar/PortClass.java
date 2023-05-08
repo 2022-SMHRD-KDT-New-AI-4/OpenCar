de38a1248d8a2b9fad86928d86e7480fc662358d
이슬이 <sylee0702@naver.com>
2023-05-08 19:57:48 +0900

    안녕(슬이)

--- app/src/main/java/com/lmj/opencar/PortClass.java
+++ app/src/main/java/com/lmj/opencar/PortClass.java
@@ -3,7 +3,9 @@ package com.lmj.opencar;
 public class PortClass {
     //static String port = "http://218.157.24.41:5001/";
 
-    static String port = "http://222.102.104.64:5001/"; // 중계 서버
+//    static String port = "http://222.102.104.64:5001/"; // 중계 서버
+    static String port = "http://172.30.1.93:5001/"; // 중계 서버
 //    static String camPort = "http://222.102.104.26:5000/"; // 라즈베리 딥러닝 서버
-    static String camPort = "http://222.102.104.64:5000/"; // 컴퓨터 딥러닝 서버
+//    static String camPort = "http://222.102.104.64:5000/"; // 컴퓨터 딥러닝 서버
+    static String camPort = "http://172.30.1.93:5000/"; // 컴퓨터 딥러닝 서버
 }
