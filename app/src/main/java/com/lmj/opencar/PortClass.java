773e6fad4d4df3d29a48e3375fe53a6e6cba35fb
kim-sun-mi <sst0612@gmail.com>
2023-05-08 11:37:48 +0900

    port번호 변경 - 선미

--- app/src/main/java/com/lmj/opencar/PortClass.java
+++ app/src/main/java/com/lmj/opencar/PortClass.java
@@ -4,6 +4,6 @@ public class PortClass {
     //static String port = "http://218.157.24.41:5001/";
 
     static String port = "http://211.63.240.23:5001/"; // 중계 서버
-//    static String camPort = "http://222.102.104.26:5000/"; // 라즈베리 딥러닝 서버
-    static String camPort = "http://222.102.104.64:5000/"; // 컴퓨터 딥러닝 서버
+    static String camPort = "http://222.102.104.26:5000/"; // 라즈베리 딥러닝 서버
+//    static String camPort = "http://222.102.104.64:5000/"; // 컴퓨터 딥러닝 서버
 }
